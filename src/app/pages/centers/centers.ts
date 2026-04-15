8d7337925a2e65319834fd305c2a4d36fd12e64e
sergtoten <casbaser013@gmail.com>
2026-04-15 19:05:25 +0100

    puesto más paginas a angular (sin funciones aun)

--- src/app/pages/centers/centers.ts
+++ src/app/pages/centers/centers.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {Header} from '../../components/header/header';
 import {Center} from '../../components/center/center';
 import {Footer} from '../../components/footer/footer';
+import {Filter} from '../../components/filter/filter';
 
 @Component({
   selector: 'app-centers',
   imports: [
     Header,
     Center,
-    Footer
+    Footer,
+    Filter
   ],
   templateUrl: './centers.html',
   styleUrl: './centers.css',
