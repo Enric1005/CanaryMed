4f6a357a5ac9474844cf6aaeacbb6efeba06ad9c
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-04-17 17:18:38 +0100

    .

--- src/app/center/center.ts
+++ src/app/center/center.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './center.html',
   styleUrl: './center.css',
 })
-export class CenterComponent {
-
-}
+export class Center {}
--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,9 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
-export class Footer {}
+export class FooterComponent implements OnInit {
+  logo = 'LOR';
+
+  socialLinks: {
+    url: string;
+    icon: string;
+  }[] = [];
+
+  sections: {
+    title: string;
+    links: {
+      text: string;
+      url: string;
+    }[];
+  }[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const DATA = this.getMockData();
+    this.loadFooter(DATA);
+  }
+
+  private loadFooter(DATA: any): void {
+    const footer = DATA.footer;
+    this.logo = footer.logo;
+
+    this.socialLinks = footer.logos.map((logo: any) => ({
+      url: logo.url,
+      icon: logo.icon
+    }));
+
+    this.sections = footer.titulos_nav.map((section: any) => ({
+      title: section.name,
+      links: section.navs.map((nav: any) => ({
+        text: nav.nav,
+        url: nav.src
+      }))
+    }));
+  }
+
+  private getMockData() {
+    return {
+      footer: {
+        logo: 'LOR',
+        logos: [
+          { url: 'https://instagram.com', icon: 'assets/ig.png' },
+          { url: 'https://facebook.com', icon: 'assets/fb.png' },
+          { url: 'https://x.com', icon: 'assets/x.png' }
+        ],
+        titulos_nav: [
+          {
+            name: 'Sección 1',
+            navs: [
+              { nav: 'Inicio', src: '/home' },
+              { nav: 'Servicios', src: '/services' }
+            ]
+          },
+          {
+            name: 'Sección 2',
+            navs: [
+              { nav: 'Contacto', src: '/contact' },
+              { nav: 'Ayuda', src: '/help' }
+            ]
+          },
+          {
+            name: 'Sección 3',
+            navs: [
+              { nav: 'Legal', src: '/legal' },
+              { nav: 'Privacidad', src: '/privacy' }
+            ]
+          }
+        ]
+      }
+    };
+  }
+}
