4c41593564c8e969dd1a82f267ecf0964ce36baa
sergtoten <casbaser013@gmail.com>
2026-04-30 22:16:34 +0100

    arreglado problema del colabora con nosotros

--- src/app/pages/work-with-us/work-with-us.ts
+++ src/app/pages/work-with-us/work-with-us.ts
@@ -1,19 +1,20 @@
-import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {Footer} from '../../components/footer/footer';
 import {Header} from '../../components/header/header';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, NgForm} from '@angular/forms';
 import {ColaboraBase} from '../../components/colabora-base/colabora-base';
 import {Router} from '@angular/router';
-import {AuthService} from '../../services/auth';
-import {Auth, User} from '@angular/fire/auth';
-import {Observable, Subscription} from 'rxjs';
+import {Auth} from '@angular/fire/auth';
+import { Subscription} from 'rxjs';
 import {AppUser} from '../profile/profile';
 import {CrudService} from '../../services/crudService';
+import { NgZone } from '@angular/core';
+import {LoadingSpinner} from '../../components/loading-spinner/loading-spinner';
 
 @Component({
   selector: 'app-work-with-us',
   standalone: true,
-  imports: [Header, Footer, FormsModule, ColaboraBase],
+  imports: [Header, Footer, FormsModule, ColaboraBase, LoadingSpinner],
   templateUrl: './work-with-us.html',
   styleUrl: './work-with-us.css',
 })
@@ -22,29 +23,32 @@ export class WorkWithUs implements OnInit, OnDestroy {
   constructor(
     private router: Router,
     private auth: Auth,
+    private ngZone: NgZone
   ) {}
 
-  user: AppUser | null = null;
+  user: AppUser | null | undefined = undefined;
   private userSub!: Subscription;
   private authUnsub!: () => void; // onAuthStateChanged devuelve una función para desuscribirse
   private crudService = inject(CrudService);
+  private cd = inject(ChangeDetectorRef);
 
   ngOnInit() {
     this.authUnsub = this.auth.onAuthStateChanged(user => {
+      this.ngZone.run(() => {  // 👈 envuelve todo aquí
+        if (this.userSub) this.userSub.unsubscribe();
 
-      // limpia la suscripción anterior si el usuario cambia
-      if (this.userSub) this.userSub.unsubscribe();
-
-      if (user) {
-        this.userSub = this.crudService
-          .getWhere<AppUser>("users", "email", "==", user.email)
-          .subscribe(res => {
-            console.log("Datos", res);
-            this.user = res[0];
-          });
-      } else {
-        this.user = null; // usuario cerró sesión
-      }
+        if (user) {
+          console.log("User found");
+          this.userSub = this.crudService
+            .getWhere<AppUser>("users", "uid", "==", user.uid)
+            .subscribe(res => {
+              this.user = res[0];
+              this.cd.detectChanges();
+            });
+        } else {
+          this.user = null;
+        }
+      });
     });
   }
 
@@ -54,8 +58,34 @@ export class WorkWithUs implements OnInit, OnDestroy {
   }
 
 
-  onSubmit(form: any) {
-    if (form.invalid) return;
+  async onSubmit(form: NgForm) {
+    if (form.invalid) {
+      alert("Completa todos los campos");
+      return;
+    }
+
+    const { institucion, correo, telefono, direccion, tipoEspecialidad, especialidad } = form.value;
+
+    try {
+      await this.crudService.add("centers", {
+        uid: this.user?.uid,
+        empresa: this.user?.name,
+        institucion,
+        correo,
+        telefono,
+        direccion,
+        tipoEspecialidad,
+        especialidad,
+        creadoEn: new Date()
+      });
+
+      alert("¡colaborador enviado correctamente!");
+      form.resetForm();
+
+    } catch (error: any) {
+      console.log("ERROR FIREBASE:", error.code, error.message);
+      alert("Error al enviar el colaborador: " + error.message);
+    }
   }
 
   goHome() {
