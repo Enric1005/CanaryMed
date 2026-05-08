7e713f9980387577b8343bb4fba0efa994184540
Enrique <enriquesosa2005@gmail.com>
2026-05-08 17:24:24 +0100

    Primer commit

--- src/app/models/center.ts
+++
@@ -1,9 +0,0 @@
-export interface CenterModel {
-  id: string;
-  name: string;
-  description: string;
-  image: string;
-  precio: 'Precio Bajo' | 'Precio Medio' | 'Precio Alto';
-  sitio: 'Ciudad' | 'Norte' | 'Sur';
-  specialities: any[];
-}
--- src/app/services/loading.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, signal } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class LoadingService {
-  isLoading = signal(false);
-
-  show() { this.isLoading.set(true); }
-  hide() { this.isLoading.set(false); }
-}
