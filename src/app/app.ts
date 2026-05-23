2be438047093e07c6a63363e294eaf7f7e72f10b
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-05-23 16:47:52 +0100

    SQLite

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { SqliteService } from './services/sqlite';
 
 @Component({
   selector: 'app-root',
   imports: [IonApp, IonRouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {}
+export class App implements OnInit {
+  constructor(private sqlite: SqliteService) {}
+
+  async ngOnInit() {
+    await this.sqlite.init();
+  }
+}
